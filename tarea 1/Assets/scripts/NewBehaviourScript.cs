eb7c6aa2d739352589a9a8eb0ab4fef6c1b391de
iuvissito <rubenalia99@gmail.com>
2021-09-17 20:42:25 +0200

    no esta terminado ejercicio 2 while

--- tarea 1/Assets/scripts/NewBehaviourScript.cs
+++ tarea 1/Assets/scripts/NewBehaviourScript.cs
@@ -4,31 +4,44 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    string saludo = "HI HUMAN ,I AM PERFECT";
-    string despedida = "ME HAS VENCIDO";
-    int number = 1;
 
+    int number = 0;
+    string saludo = "hi peter";
+    string respuesta = "BIENVENIDO A LOS JUEGOS DEL HAMBRE";
+    string respuesta1 = "¡¡¡SOY INEVITABLE!!!";
+    string despedida = "HA LLEGADO EL FINAL INSENSATO";
     // Start is called before the first frame update
     void Start()
     {
+        while (number < 500)
+        { print(saludo);
+            number++;
 
-        
-        
+            if (number == 100)
+            {
+                print(respuesta);
+                number++;
+            }
+
+            else if (number == 200)
+            {
+
+                print(respuesta1);
+            }
 
 
+
+        else {
+                print(despedida);
+            }
+
+        }
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (number <100)
-        { print(saludo);
-            number++;
-        }
         
-        else { 
-            print(despedida);
-        }
     }
 }
