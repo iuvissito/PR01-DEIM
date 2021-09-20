9718cfd34f94a2eed832d6f53eb37d3c7998f822
iuvissito <rubenalia99@gmail.com>
2021-09-20 18:42:25 +0200

    2 b y c for
    hecho el ejercicio con for

--- tarea 1/Assets/scripts/NewBehaviourScript.cs
+++ tarea 1/Assets/scripts/NewBehaviourScript.cs
@@ -8,20 +8,24 @@ public class NewBehaviourScript : MonoBehaviour
     int number = 0;
     string saludo = "hi peter";
     string respuesta = "BIENVENIDO A LOS JUEGOS DEL HAMBRE";
-    string respuesta1 = "¡¡¡SOY INEVITABLE!!!";
+    string respuesta1 = "¡¡¡SOY INEVITABLE !!!";
     string despedida = "HA LLEGADO EL FINAL INSENSATO";
+
+
     // Start is called before the first frame update
     void Start()
     {
-        while (number < 500)
-        {
-            print(saludo);
-            number++;
+        for ( int number = 0;  number < 500; number++)
+
+
+        
+        { print(saludo);
+            
 
             if (number == 100)
             {
                 print(respuesta);
-                number++;
+                
             }
 
             else if (number == 200)
@@ -32,14 +36,13 @@ public class NewBehaviourScript : MonoBehaviour
 
 
 
-            else
-            {
-
+        else {
+                
             }
 
         }
         print(despedida);
-    }
+}
 
     // Update is called once per frame
     void Update()
