38e12206a4e01aa652b0dfb80acd0c1211bb73c9
ruben <rubenalia99@gmail.com>
2021-09-24 18:00:14 +0200

    reloj

--- tarea 1/Assets/scripts/NewBehaviourScript.cs
+++ tarea 1/Assets/scripts/NewBehaviourScript.cs
@@ -5,29 +5,57 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
 
-
-
+    int h = 0;
+    int m = 0;
+    int s = 0;
+    int day = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine("Cuenta");
+        StartCoroutine("reloj");
     }
-    IEnumerator Cuenta()
+    IEnumerator reloj()
     {
-        for (int n = 20; n >= 0; n--)
+        while (true)
         {
-            print(" contador :" + n);
+            print("la hora es " + h + "hour" + m + "min" + s + "seg");
+            s++;
             yield return new WaitForSeconds(1f);
+
+            if (s > 59)
+            {
+                s = 0;
+                m++;
+            }
+            if (m > 59)
+            {
+                m = 0;
+                h++;
+            }
+            if (h > 23)
+            {
+                h = 0;
+                day++;
+                print("BUENOS DIAS es el dia: " + day);
+            }
+
+
 
 
         }
+
+        
     }
-    // Update is called once per frame
+ // Update is called once per frame
     void Update()
-    {
-
-
-
+    { 
+        
     }
+    
+  
+
+  
+
+
 }
